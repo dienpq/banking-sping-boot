69a862a3ee8a4ae34d5747e8ea48f770dca25d90
phandien <dienpq1604@gmail.com>
2023-03-10 19:27:15 +0700

    create model

--- src/main/java/banking/responsitories/UserRespository.java
+++ src/main/java/banking/responsitories/UserRespository.java
@@ -1,8 +1,11 @@
 package banking.responsitories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import banking.models.User;
 
+@Repository
 public interface UserRespository extends CrudRepository<User, Long> {
+
 }
